a146e37ba5159cb6f807933c73854b185365cba5
Anton Astashov <anton.astashov@gmail.com>
2014-08-02 13:58:28 -0700

    Remove cache when deleting an item

--- src/tixi/mutators/delete.cljs
+++ src/tixi/mutators/delete.cljs
@@ -9,5 +9,6 @@
   (when (not-empty ids)
     (mu/snapshot!)
     (doseq [id ids]
+      (swap! d/data update-in [:cache] dissoc id)
       (ml/delete-from-locks! id (d/completed-item id))
       (msh/update-state! update-in [:completed] dissoc id))))
